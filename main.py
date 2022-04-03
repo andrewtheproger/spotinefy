74ec30d1bf9e361e59eb6981fb02c9ef5edfe4ea
andrewtheproger <bokhonov7@gmail.com>
2022-04-03 19:19:42 +0400

    added ability to get info about songs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+from re import S
 from mutagen.mp3 import MP3
 from data import db_session
 from flask import Flask, render_template, request, url_for, redirect
 from data.songs import Song
 from data.authors import Author
-from flask import send_file, send_from_directory, safe_join, abort
+from data.links import Link
+from flask import send_from_directory, abort
 import xmltodict
 from pprint import pprint
 import json
@@ -16,14 +18,18 @@ app.config["CLIENT_SONGS"] = "./songs/"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
-
-def get_text(song, author):
+@app.route("/get-song-text/<id>")
+def get_song_text(id):
+    song = json.loads(get_song_data(id))["name"]
+    author = db_sess.query(Author).filter(Author.id == json.loads(get_song_data(id))["authors"][-1]).first().name
     responce = requests.get(
         f"http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist={author}&song={song}"
     )
-    return json.loads(json.dumps(xmltodict.parse(responce.content)))[
+    data = json.loads(json.dumps(xmltodict.parse(responce.content)))[
         "ArrayOfSearchLyricResult"
     ]["SearchLyricResult"][0]
+    responce = requests.get(f"http://api.chartlyrics.com/apiv1.asmx/GetLyric?lyricId={data['LyricId']}&lyricCheckSum={data['LyricChecksum']}")
+    return json.loads(json.dumps(xmltodict.parse(responce.content)))["GetLyricResult"]["Lyric"]
 
 
 @app.route("/")
@@ -56,10 +62,9 @@ def add_music():
     return render_template("add_music.html")
 
 
-@app.route("/get-song/<id>")
-def get_song(id):
+@app.route("/get-song-file/<id>")
+def get_song_file(id):
     try:
-        pprint(db_sess.query(Song).filter(Song.id == id).first().name)
         return send_from_directory(
             app.config["CLIENT_SONGS"],
             db_sess.query(Song).filter(Song.id == id).first().name + ".mp3",
@@ -67,6 +72,18 @@ def get_song(id):
         )
     except FileNotFoundError:
         abort(404)
+
+
+@app.route("/get-song-data/<id>")
+def get_song_data(id):
+    res = {}
+    song = db_sess.query(Song).filter(Song.id == id).first()
+    res["authors"] = list(map(lambda x: x.author_id, db_sess.query(Link).filter(Link.song_id == id)))
+    print(res["authors"])
+    res["year"] = song.year
+    res["duration"] = song.duration
+    res["name"] = song.name
+    return json.dumps(res)
 
 
 @app.route("/add-artist", methods=["POST", "GET"])
